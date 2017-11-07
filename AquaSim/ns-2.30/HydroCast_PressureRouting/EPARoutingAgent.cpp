c973437fad5ea32542e8f64a23c83f622cf5e861
DUNHAOZHONG <dunhaozhong@gmail.com>
2017-11-07 17:27:28 -0500

    Add new protocol based on max EPA. TODO: Timer of sending beacon, command function, sending packet

---
+++ AquaSim/ns-2.30/HydroCast_PressureRouting/EPARoutingAgent.cpp
@@ -0,0 +1,172 @@
+/*
+ * EPARouting.cpp
+ *
+ *  Created on: Nov 7, 2017
+ *      Author: ZDH
+ */
+
+#include <HydroCast_PressureRouting/EPARoutingAgent.h>
+
+EPARoutingAgent::EPARoutingAgent() {
+	// TODO Auto-generated constructor stub
+
+}
+
+EPARoutingAgent::~EPARoutingAgent() {
+	// TODO Auto-generated destructor stub
+}
+
+static class EPARoutingAgentClass: public TclClass {
+public:
+	EPARoutingAgentClass() :
+			TclClass("Agent/EPARouting") {
+	}
+	TclObject* create(int, const char* const *) {
+		return (new EPARoutingAgent);
+	}
+} class_EPARoutingAgent;
+
+
+class EPAHeaderClass: public PacketHeaderClass {
+public:
+	EPAHeaderClass() :
+			PacketHeaderClass("PacketHeader/EPARouting", sizeof(packet_hdr)) {
+		bind_offset(&packet_hdr::offset_);
+	}
+} class_EPAHeader;
+
+//=============================NeighbTable=========================================
+void NeighbTable::delete_ent(const NeighbEnt *e) {
+	if(num_ents<=0)
+		return;
+	if( neighbTable.find(e->net_id) != neighbTable.end() ){
+		delete neighbTable[e->net_id];
+		neighbTable.erase(e->net_id);
+		num_ents--;
+	}
+	return;
+}
+
+void NeighbTable::add_ent(const NeighbEnt *e) {
+	if( neighbTable.find(e->net_id) != neighbTable.end() ){
+		neighbTable.erase(e->net_id);
+	}
+	neighbTable[e->net_id] = e;
+	return;
+}
+
+
+
+//=============================EPARoutingAgent=========================================
+void EPARoutingAgent::init(void){
+	BEACON_RESCHED;
+}
+
+Packet* EPARoutingAgent::makeBeacon(){
+	Packet *p = allocpkt();
+	assert(p != NULL);
+
+	hdr_ip *iph = hdr_ip::access(p);
+	hdr_cmn *cmh = hdr_cmn::access(p);
+	packet_hdr *packetHrd = packet_hdr::access(p);
+
+	cmh->next_hop_ = IP_BROADCAST;
+	cmh->direction() = hdr_cmn::DOWN;
+	cmh->size() = packetHrd->size() + IP_HDR_LEN;
+//	cmh->ptype = PT_MAX_EPA;
+
+	iph->daddr() = IP_BROADCAST << Address::instance().nodeshift();
+	iph->saddr() = mn_->address();
+	iph->daddr() = IP_BROADCAST;
+	iph->dport() = EPA_PORT;
+
+	mn_->getLoc(&(packetHrd->x), &(packetHrd->y), &(packetHrd->z));
+	packetHrd->mode = EPA_BEACON;
+	packetHrd->mhops = 1;
+	packetHrd->neighb_1hop = getNeighbors_1hop();
+	return p;
+}
+
+void EPARoutingAgent::sendBeacon(void) {
+	Packet *p = makeBeacon();
+	hdr_cmn *cmh = hdr_cmn::access(p);
+
+	if (p) {
+		assert(!HDR_CMN(p)->xmit_failure_);
+		if (cmh->direction() == hdr_cmn::UP)
+			cmh->direction() = hdr_cmn::DOWN;
+		Scheduler::instance().schedule(ll, p, 0);
+	} else {
+		fprintf(stderr, "ERROR: Can't make new beacon!\n");
+		abort();
+	}
+}
+
+std::vector<NeighbEnt> EPARoutingAgent::getNeighbors_1hop(){
+	std::vector<NeighbEnt> neighb_1hop;
+	map<nsaddr_t,NeighbEnt*>::iterator iter;
+	for(iter = neighbTable_1hop.neighbTable.begin(); iter != neighbTable_1hop.neighbTable.end(); ++iter){
+		neighb_1hop.push_back(*(iter->second));
+	}
+	return neighb_1hop;
+}
+
+
+void EPARoutingAgent::recv(Packet *p, Handler *){
+	hdr_ip *iph = hdr_ip::access(p);
+	hdr_cmn *cmh = hdr_cmn::access(p);
+	packet_hdr *packetHrd = packet_hdr::access(p);
+
+//	nsaddr_t src = Address::instance().get_nodeaddr(iph->saddr());
+//	nsaddr_t dst = Address::instance().get_nodeaddr(iph->daddr());
+
+	switch(packetHrd->mode){
+	case EPA_BEACON:
+		nsaddr_t sender_id = packetHrd->node_id;
+		NeighbEnt* newEnt = new NeighbEnt(sender_id);
+		newEnt->x = packetHrd->x;
+		newEnt->y = packetHrd->y;
+		newEnt->z = packetHrd->z;
+		newEnt->hop = 1;
+		newEnt->expireTime = EXPIRE_TIME;
+		neighbTable_1hop.add_ent(newEnt);
+
+		//Add 2-hop nodes
+		if( neighbTable_2hop.find(sender_id) == neighbTable_2hop.end() ){
+			NeighbTable *neighbTalbe = new NeighbTable();
+			int len = packetHrd->neighb_1hop.size();
+
+			for(int i = 0; i < len; i++){
+				NeighbEnt* newEnt = new NeighbEnt(packetHrd->neighb_1hop[i].net_id);
+				newEnt->x = packetHrd->neighb_1hop[i].x;
+				newEnt->y = packetHrd->neighb_1hop[i].y;
+				newEnt->z = packetHrd->neighb_1hop[i].z;
+				newEnt->hop = 2;
+				newEnt->expireTime = EXPIRE_TIME;
+				neighbTalbe->add_ent(newEnt);
+			}
+
+			neighbTable_2hop[sender_id] = neighbTalbe;
+		}
+		break;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+void BeaconHandler::handle(Event *e) {
+	a_->sendBeacon();
+}
+
+
+
+
+
+
---
+++ AquaSim/ns-2.30/HydroCast_PressureRouting/EPARoutingAgent.h
@@ -0,0 +1,154 @@
+/*
+ * EPARouting.h
+ *
+ *  Created on: Nov 7, 2017
+ *      Author: ZDH
+ */
+
+#ifndef EPAROUTINGAGENT_H_
+#define EPAROUTINGAGENT_H_
+
+#include <deque>
+#include <map>
+#include <vector>
+#include <stdarg.h>
+
+#include <object.h>
+#include <agent.h>
+#include <trace.h>
+#include <packet.h>
+#include <mac.h>
+#include <mobilenode.h>
+#include <classifier-port.h>
+#include <scheduler.h>
+#include <random.h>
+#include <tcl.h>
+#include <ll.h>
+#include <cmu-trace.h>
+
+
+
+#define	EPA_PORT		0xFF
+#define	EPA_BEACON		1
+#define EXPIRE_TIME     120
+
+#define	BEACON_RESCHED  beacon_timer_->resched(30);
+
+class EPARoutingAgent;
+
+//============================Timer Start=======================================
+class AgentTimer : public TimerHandler {
+public:
+	AgentTimer(EPARoutingAgent *a) { a_ = a; }
+	virtual void expire(Event *e) = 0;
+
+protected:
+	EPARoutingAgent *a_;
+};
+
+class BeaconHandler : public Handler {
+public:
+	BeaconHandler(EPARoutingAgent *a) { a_ = a; }
+	virtual void handle(Event *e);
+
+private:
+	EPARoutingAgent *a_;
+};
+
+class BeaconTimer : public AgentTimer {
+public:
+	BeaconTimer(EPARoutingAgent *a) : AgentTimer(a) {}
+	virtual void expire(Event *e);
+};
+
+//============================Timer end=========================================
+
+
+//=============================NeighbTable Start====================================
+class NeighbEnt {
+public:
+	NeighbEnt(nsaddr_t net_id) : x(0.0), y(0.0), z(0.0), hop(1), expireTime(0) {
+		this->net_id = net_id;
+	}
+	// the agent is used for timer object
+
+	double x, y, z;		// location of neighbor, actually we only need depth info
+	nsaddr_t net_id;    // IP of neighbor
+	int hop;		    // indicates the hop of neighbor
+	double expireTime;  // timer for expiration of neighbor
+};
+
+class NeighbTable {
+public:
+	NeighbTable();
+	~NeighbTable();
+
+	void dump(void);
+	void delete_ent(const NeighbEnt *e);        	// delete an neighbor
+	void add_ent(const NeighbEnt *e);     	// add an neighbor
+
+private:
+	int num_ents;       // number of entries in use
+	std::map<nsaddr_t, NeighbEnt*> neighbTable;
+};
+//=============================NeighbTable End====================================
+
+
+//=============================Packet header Start====================================
+class packet_hdr {
+public:
+	static int offset_;		// offset of this header
+	static int& offset() { return offset_; }
+	static packet_hdr* access(const Packet *p) {
+		return (packet_hdr*)p->access(offset_);
+	}
+
+	nsaddr_t source;    //the source id of message
+	nsaddr_t node_id;   // IP
+	double x, y, z;     // location
+	int packetID;       // unique id for packet
+	int mode;           //packet mode
+	int mhops;		    // max # of hops to broadcast
+	std::vector<NeighbEnt> neighb_1hop;
+
+	// get the header size
+	int size()
+	{ 	int sz;
+		sz = 4 * sizeof(int);
+		sz += 3 * sizeof(double);
+		return sz;
+	}
+};
+//=============================Packet header End====================================
+
+//=============================EPARoutingAgent Start====================================
+class EPARoutingAgent : public Agent {
+public:
+	EPARoutingAgent();
+	virtual ~EPARoutingAgent();
+
+	virtual int command(int argc, const char*const* argv);
+	virtual void recv(Packet *, Handler *);
+
+protected:
+	NeighbTable neighbTable_1hop;
+	std::map<nsaddr_t, NeighbTable> neighbTable_2hop;  //store my neighbor's neighbors
+	NsObject *ll;				// our link layer output
+	MobileNode* mn_;			// MobileNode associated with the agent
+	BeaconTimer *beacon_timer_;		// beacon timer
+
+	void init(void);
+	void sendBeacon(void);
+	Packet* makeBeacon();    //make beacon header
+	std::vector<NeighbEnt> getNeighbors_1hop();  //neighbors with 1 hop
+};
+//=============================EPARoutingAgent End====================================
+
+#endif /* EPAROUTINGAGENT_H_ */
+
+
+
+
+
+
+
